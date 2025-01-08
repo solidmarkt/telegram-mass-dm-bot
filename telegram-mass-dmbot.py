f10902a3364e0c9100574e2492fcebbb808e2456
Solid Marketing <189283094+solidmarkt@users.noreply.github.com>
2025-01-08 20:30:34 +0300

    Add files via upload

---
+++ telegram-mass-dmbot.py
@@ -0,0 +1,13 @@
+#This file has no features, please review the README.MD file.
+
+print("Please read README.MD file for details!")
+
+print("Please read README.MD file for details!")
+
+print("Please read README.MD file for details!")
+
+print("Please read README.MD file for details!")
+
+print("Please read README.MD file for details!")
+
+print("Telegram DM Bot")
